c0897cfc306f6326ab70bd26bdc983c2ca454b61
Daniel Miguel Medinas Mateo <dmedinasm@gmail.com>
2022-09-21 23:13:34 -0700

    Consumiendo primera API REST, reto con Async/wait

--- src/Primer consumo API/index.js
+++ src/Primer consumo API/index.js
@@ -1,11 +1,24 @@
-console.log("Hello World");
+const url = "https://api.thedogapi.com/v1/images/search";
+
+const boton = document.getElementById("boton");
+
+boton.addEventListener("click", changePhoto);
+
+async function fetchData(urlApi) {
+  const response = await fetch(urlApi);
+  const data = await response.json();
+  const img = document.querySelector("img");
+  img.src = data[0].url;
+};
+
+async function changePhoto(){
+   try{
+      await fetchData(url);
+   }catch(error){
+      console.error(error);
+   }
+};
+
+changePhoto();
 
 
-const url = 'https://api.thedogapi.com/v1/images/search';
-
-fetch(url)
-   .then(res => res.json())
-   .then(data => {
-    const img = document.querySelector('img')
-    img.src = data[0].url;
-   });
