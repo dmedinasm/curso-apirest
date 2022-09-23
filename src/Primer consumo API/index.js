325be790c11c8db2c02a4d638e561e65e0b36aef
Daniel Miguel Medinas Mateo <dmedinasm@gmail.com>
2022-09-22 23:02:21 -0700

    Endpoints y Query parameters

--- src/Primer consumo API/index.js
+++ src/Primer consumo API/index.js
@@ -1,24 +1,21 @@
-const url = "https://api.thedogapi.com/v1/images/search";
+const urlApi= "https://api.thedogapi.com/v1/images/search?limit=3";
 
-const boton = document.getElementById("boton");
-
-boton.addEventListener("click", changePhoto);
-
-async function fetchData(urlApi) {
+async function reload() {
+   try {
   const response = await fetch(urlApi);
   const data = await response.json();
-  const img = document.querySelector("img");
-  img.src = data[0].url;
-};
-
-async function changePhoto(){
-   try{
-      await fetchData(url);
-   }catch(error){
+  console.log(data);
+  const img1 = document.getElementById("img1");
+  const img2 = document.getElementById("img2");
+  const img3 = document.getElementById("img3");
+   img1.src = data[0].url;
+   img2.src = data[1].url;
+   img3.src = data[2].url;
+   } catch(error){
       console.error(error);
    }
 };
 
-changePhoto();
 
+reload();
 
