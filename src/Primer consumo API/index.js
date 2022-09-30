f9c601e9f9eabc8a4bf6723ec348d6fd1045e007
Daniel Miguel Medinas Mateo <dmedinasm@gmail.com>
2022-09-30 16:37:09 -0700

    GET:Leyendo dogs favoritos, reto objeto status codes

--- src/Primer consumo API/index.js
+++ src/Primer consumo API/index.js
@@ -1,17 +1,32 @@
-const API_URL_RANDOM =
-  "https://api.thedogapi.com/v1/images/search?limit=2&api_key=live_Je2iJayLCJmORfO2DRBgtpj4HhyrRmjiiyeLO9MtQVkxryEFEsjd85f7xWVIzB2s";
-//const API_URL_FAVOURITES = "https://api.thedogapi.com/v1/favourites?limit=2";
+const API_URL_RANDOM = "https://api.thedogapi.com/v1/images/search?limit=2&api_key=live_Je2iJayLCJmORfO2DRBgtpj4HhyrRmjiiyeLO9MtQVkxryEFEsjd85f7xWVIzB2s";
+const API_URL_FAVOURITES = "https://api.thedogapi.com/v1/favourites?limit=2";
+const STATUS_CODES = [
+   {'message':'Unauthorized', 'status': 401},
+   {'message':'Payment_Required', 'status': 402},
+   {'message':'Forbidden', 'status': 403},
+   {'message':'Not_Found', 'status': 404},
+   {'message':'Internal_Server_Error','status': 500},
+   {'message':'Not_Implemented','status': 501},
+   {'message':'Bad_Gateway','status': 502},
+   {'message':'Service_Unavaliable','status': 503},
+   {'message':'Gateway_Timeout','status': 504},
+]
+
+const compareCodes = (value) => {
+  return STATUS_CODES.find(item => item.status === value);
+}
 
 const divError = document.getElementById("Error");
 console.log(divError);
 async function loadRandomDogs() {
   const response = await fetch(API_URL_RANDOM);
-  const data = await response.json();
-  console.log(data);
-  console.log("Random");
   if (response.status !== 200) {
     divError.innerHTML = "Hubo un error: " + response.status;
   } else {
+    const data = await response.json();
+    console.log(data);
+    console.log("Random");
+
     const img1 = document.getElementById("img1");
     const img2 = document.getElementById("img2");
 
@@ -20,14 +35,16 @@ async function loadRandomDogs() {
   }
 }
 
-const API_URL_FAVOURITES = "https://api.thedogapi.com/v1/favourites?limit=2";
-
 async function loadFavoritesDogs() {
   const response = await fetch(API_URL_FAVOURITES);
-  const data = await response.text();
   if (response.status !== 200) {
-    divError.innerHTML = "Hubo un error: " + response.status + data ;
-  } 
+    divError.innerHTML = "Hubo un error: " + response.status + " " + compareCodes(response.status).message;
+  } else {
+    const data = await response.json();
+    console.log('Favorites');
+    console.log(data);
+
+  }
 }
 
 
