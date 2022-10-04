67db25e3b7e3670d897d588c27efae69d2391a09
Daniel Miguel Medinas Mateo <dmedinasm@gmail.com>
2022-10-04 00:21:59 -0700

    Consultas a la API para escribir HTML dinamico

--- src/Primer consumo API/index.js
+++ src/Primer consumo API/index.js
@@ -31,9 +31,16 @@ async function loadRandomDogs() {
 
     const img1 = document.getElementById("img1");
     const img2 = document.getElementById("img2");
+    const btn1 = document.getElementById('btn1');
+    const btn2 = document.getElementById('btn2');
+
 
     img1.src = data[0].url;
     img2.src = data[1].url;
+// No poner la funcion saveFavorites directamente ya que las ejecuta no mas cargue la funcion
+    btn1.onclick = () => saveFavouriteDogs(data[0].id);
+    btn2.onclick = () => saveFavouriteDogs(data[1].id);
+
   }
 }
 
@@ -41,18 +48,29 @@ async function loadFavouriteDogs() {
   const response = await fetch(API_URL_FAVOURITES);
   if (response.status !== 200) {
     divError.innerHTML =
-      "Hubo un error: " +
-      response.status +
-      " " +
-      compareCodes(response.status).message;
+      "Hubo un error: " + response.status + " " + compareCodes(response.status).message;
   } else {
     const data = await response.json();
     console.log("Favorites");
     console.log(data);
+    data.forEach(dogs => {
+      const section = document.getElementById('favoriteDogs');
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      const btn = document.createElement('button');
+      const btnText = document.createTextNode('Sacar al perro de favoritos');
+
+      img.src = dogs.image.url
+      img.width = 150;
+      btn.appendChild(btnText);
+      article.appendChild(img);
+      article.appendChild(btn);
+      section.appendChild(article);
+    });
   }
 }
 
-async function saveFavouriteDogs() {
+async function saveFavouriteDogs(id) {
   const res = await fetch(API_URL_FAVOURITES, {
     method: "POST",
     headers: {
@@ -60,14 +78,14 @@ async function saveFavouriteDogs() {
     },
     //Metodo stringify para que el backend pueda entender ya que el backend puede estar en otro lenguaje y se especifica un string
     body: JSON.stringify({
-      image_id: '1Kri0eY18B',
+      image_id: id,
     }),
   });
-  
+
   console.log("Save");
   console.log(res);
   if (res.status !== 200) {
-    divError.innerHTML = "Hubo un error: " + res.status + await res.text();
+    divError.innerHTML = "Hubo un error: " + res.status + (await res.text());
   }
   const data = await res.json();
 }
